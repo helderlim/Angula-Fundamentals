861262946731ac68a907042c834239e3c85fede3
helderlim <helderfelima16@gmail.com>
2021-12-23 21:15:14 -0300

    fix: acessing static files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
+  //pipe de componente ou diretiva -> declarations
   declarations: [
     EventsAppComponent
   ],
+  //importar outros módulos -> imports
   imports: [
     BrowserModule
   ],
+  //providers é para declarar services
   providers: [],
   bootstrap: [EventsAppComponent]
 })
