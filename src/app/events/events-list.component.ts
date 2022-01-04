8bc5606376b398e9dbdd1a713d4bffe8cc771664
helderlim <helderfelima16@gmail.com>
2022-01-03 22:41:26 -0300

    using template varuables to interact with child components

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,8 +6,9 @@ import { Component } from "@angular/core";
     <div>
         <h1>Upcoming Angular Events</h1>
         <hr>
-        <event-thumbnail (eventClick)="handleEventClicked($event)"
-        [event]="event1"></event-thumbnail>
+        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+        <h3>{{thumbnail.someProperty}}</h3>
+        <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo </button>
     </div>
 `
 })
@@ -25,8 +26,5 @@ export class EventsListComponent {
             city: 'London',
             country: 'Englad'
         }
-    }
-    handleEventClicked(data: any){
-        console.log('received:', data);
     }
 }
