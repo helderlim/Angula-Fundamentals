c941f12b02d6106f2ad1051f99bbfb99fffb7b42
helderlim <helderfelima16@gmail.com>
2022-01-05 18:11:37 -0300

    adding a site header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   //pipe de componente ou diretiva -> declarations
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   //importar outros módulos -> imports
   imports: [
