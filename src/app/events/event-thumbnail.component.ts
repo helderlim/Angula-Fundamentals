607decc97d7a93c5a82bbeec6ce436cf301c5c80
helderlim <helderfelima16@gmail.com>
2022-01-05 21:18:55 -0300

    using ngIf

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,9 +8,11 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
         <div>Date: {{event?.date}}</div>
         <div>TIme: {{event?.time}}</div>
         <div>Price: \${{event?.price}}</div>
-        <div>
+        <div *ngIf="event?.location">
             <span>Location: {{event?.location?.address}}</span>
             <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
         </div>
     </div>
     `,
