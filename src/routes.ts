dadc617bc249b66081f71b49c25e720e80745c3f
helderlim <helderfelima16@gmail.com>
2022-01-14 18:10:52 -0300

    fix acessing route parameters

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,8 @@ import { Routes } from "@angular/router";
 import { EventDetailsComponent } from "./app/events/event-details/event-details.component";
 import { EventsListComponent } from "./app/events/events-list.component";
 
-export const appRoutes: Routes = [
+export const appRoutes:Routes = [
     { path: 'events', component: EventsListComponent },
-    { path: 'evetns/:id', component: EventDetailsComponent},
+    { path: 'events/:id', component: EventDetailsComponent},
     { path: '', redirectTo: '/events', pathMatch: 'full'}
 ]
