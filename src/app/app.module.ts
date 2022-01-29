a3b334fac60c2d093362cd67e90896b003ca5cae
helderlim <helderfelima16@gmail.com>
2022-01-29 15:01:23 -0300

    guarding against route activation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from 'src/routes';
+import { Error404Component } from './errors/404.components';
 
 import { EventsAppComponent } from './events-app.component';
 import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
@@ -19,7 +21,8 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     EventDetailsComponent,
     NavBarComponent,
-    CreateEventComponent
+    CreateEventComponent,
+    Error404Component
   ],
   //importar outros módulos -> imports
   imports: [
@@ -27,7 +30,7 @@ import { NavBarComponent } from './nav/navbar.component';
     RouterModule.forRoot(appRoutes)
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  providers: [EventService],
+  providers: [EventService, EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
